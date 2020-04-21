28f6185d9c0964a87fb9c54b8cb00780b11c5b09
kkanr041 <kkant041@gmail.com>
2020-04-21 15:07:35 +0530

    kkant041

---
+++ core/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class prodDir(models.Model):
+    """Model definition for prodDir."""
+
+    # TODO: Define fields here
+    prodName = models.CharField(max_length=100)
+    numOfPieces = models.PositiveIntegerField(default=0)
+    buyingPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    sellingPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    totalValue = models.DecimalField
+
+    class Meta:
+        """Meta definition for prodDir."""
+
+        verbose_name = 'prodDir'
+        verbose_name_plural = 'prodDirs'
+
+    def __str__(self):
+        """Unicode representation of prodDir."""
+        return prodDir.prodName, prodDir.numOfPieces, prodDir.buyingPrice, prodDir.sellingPrice, prodDir.totalValue
+
+class transHis(models.Model):
+    """Model definition for transHis."""
+
+    # TODO: Define fields here
+    prodId = models.ForeignKey(prodDir, on_delete=models.CASCADE)
+    transType = models.CharField(max_length=10)
+    price = models.ForeignKey(prodDir, on_delete=models.CASCADE)
+
+    class Meta:
+        """Meta definition for transHis."""
+
+        verbose_name = 'transHis'
+        verbose_name_plural = 'transHis'
+
+    def __str__(self):
+        """Unicode representation of transHis."""
+        return transHis.prodId, transHis.price, transHis.transType
+
